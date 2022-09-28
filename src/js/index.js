380145a0b9a88c9ee25df212e26cba0739f0cf34
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-09-28 21:40:52 +0000

    Progress with the function SimpleCounter

--- src/js/index.js
+++ src/js/index.js
@@ -9,5 +9,19 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+
+function SimpleCounter (props)b {
+    return <div className ="counterwhole">
+      <div className = "calender"></div>
+      <div className ="position4"></div>
+      <div className ="position3"></div>
+      <div className ="position2"></div>
+      <div className ="position1"></div>
+    </div>;
+}
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render (
+    <SimpleCounter/>
+    document.querySelector ('app')
+)
