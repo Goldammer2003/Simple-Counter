d586b44f40ae0db81c86f54d69bc0730cbd3af75
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-09-29 01:04:32 +0000

    Alternative way

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
 import React from "react";
-
+import PropTypes from 'prop-types';
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+function SimpleCounter (props) {
+    return (
+    <div className ="counterwhole">
+      <div className = "calender">
+        <i className ="far fa-clock"></i>
+      </div>
+      <div className ="position4">{props.digitFour}</div>
+      <div className ="position3">{props.digitThree}</div>
+      <div className ="position2">{props.digitTwo}</div>
+      <div className ="position1">{props.digitOne}</div>
+    </div>)
+}
+SimpleCounter.propTypes = {
+    digitFour:PropTypes.array,
+    digitThree:PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne:PropTypes.number,
 };
 
-export default Home;
+let counter = 0;
+setInterval (function () {
+    // const four= Math.floor (counter/1000);
+    // const three=Math.floor (counter/100);
+    // const two = Math.floor (counter/10);
+    // const one = Math.floor (counter/1)
+
+    // counter++;
+
+console.log ("i am in set interval")
+//render your react application
+
+
+
+},1000)
+
+export default SimpleCounter
--- src/js/index.js
+++ src/js/index.js
@@ -1,54 +1,20 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap";
-import fontawesome from "@fortawesome/fontawesome";
-import faFlock from "@fortawesome/fontawesome-free-regular/faClock";
-import './icons.js'
-import PropTypes from "proptypes";
 
-fontawesome.config = {
-    autoReplaceSvg: 'nest'
-}
-fontawesome.library.add (faClock);
+//fontawesome.config = {
+ //   autoReplaceSvg: 'nest'
+//}
+//fontawesome.library.add (faClock);
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import SimpleCounter from "./component/home.jsx";
 
 
-function SimpleCounter (props) {
-    return (
-    <div className ="counterwhole">
-      <div className = "calender">
-        <i className ="far fa-clock"></i>
-      </div>
-      <div className ="position4">{props.digitFour}</div>
-      <div className ="position3">{props.digitThree}</div>
-      <div className ="position2">{props.digitTwo}</div>
-      <div className ="position1">{props.digitOne}</div>
-    </div>)
-}
-SimpleCounter.propTypes = {
-    digitFour:PropTypes.array,
-    digitThree:PropTypes.number,
-    digitTwo: PropTypes.number,
-    digitOne:PropTypes.number,
-};
 
-let counter = 0;
-setIntervall (function () {
-    const four= Math.floor (counter/1000);
-    const three=Math.floor (counter/100);
-    const two = Math.floor (counter/10);
-    const one = Math.floor (counter/1)
-
-    counter++;
-});
-//render your react application
 ReactDOM.render (
-    <SimpleCounter digitOne ={one} digittwo ={two} digitthree ={three} digitfour ={four} />,
+    <SimpleCounter digitOne ={one} digitTwo ={two} digitThree ={three} digitFour ={four} />,
     document.querySelector ('app')
 );
